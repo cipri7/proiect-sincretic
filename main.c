676e8ca295a6c7b3638af6ff0e39a70e50a24b58
cipri7 <102433321+cipri7@users.noreply.github.com>
2023-10-13 09:07:24 +0300

    Implement Hanoi algoritm

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void Hanoi(int n, char source, char aux, char destination){
+    if(n == 1){
+        printf("Move disk 1 from %c to %c \n", source, destination);
+        return;
+    }
+
+    Hanoi(n-1, source, destination, aux);
+    printf("Move disk %d from %c to %c \n", n, source, destination);
+    Hanoi(n-1, aux, source, destination);
+}
+
 int main(){
     printf("Hanoi Towers\n");
 
